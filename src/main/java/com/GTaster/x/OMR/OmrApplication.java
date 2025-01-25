f14e096d2c7d5cb2a29bc070e9533fed4186153a
Atharva Gulve <atharvagulve1306@gmail.com>
2025-01-25 17:23:34 +0530

    first commit

---
+++ src/main/java/com/GTaster/x/OMR/OmrApplication.java
@@ -0,0 +1,13 @@
+package com.GTaster.x.OMR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GTaster/x/OMR/OmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GTaster.x.OMR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
